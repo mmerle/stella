64458167d4cd541d63f73f63c3463c90a7036f41
mmerle <35971019+mmerle@users.noreply.github.com>
2024-05-16 19:04:03 -0400

    add `astro-sitemap`

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,20 @@ import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
 import partytown from '@astrojs/partytown';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   site: process.env.WEBSITE_URL,
   output: 'server',
-  prefetch: true,
+  prefetch: {
+    defaultStrategy: 'viewport',
+  },
   adapter: cloudflare({
     imageService: 'passthrough',
     platformProxy: {
       enabled: true,
     },
   }),
-  integrations: [react(), partytown()],
+  integrations: [react(), partytown(), sitemap()],
 });
